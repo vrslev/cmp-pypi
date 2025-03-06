f503f700fa730200d6a6826c36194cb826776fe2
Dom <waltersdom@gmail.com>
2025-03-06 13:50:30 +0000

    chore: fix tab characters

--- lua/cmp-pypi/init.lua
+++ lua/cmp-pypi/init.lua
@@ -2,23 +2,23 @@ local source = {}
 
 ---@return string
 function source:get_debug_name()
-	return "pypi"
+    return "pypi"
 end
 
 ---@return boolean
 function source:is_available()
-	local filename = vim.fn.expand("%:t")
-	return filename == "pyproject.toml"
+    local filename = vim.fn.expand("%:t")
+    return filename == "pyproject.toml"
 end
 
 ---@return string
 function source:get_keyword_pattern()
-	return [[\k\+]]
+    return [[\k\+]]
 end
 
 ---@return string[]
 function source:get_trigger_characters()
-	return {
+    return {
         -- Part way through a version
         ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
         -- Case 1
@@ -31,42 +31,42 @@ end
 ---@param params cmp.SourceCompletionApiParams
 ---@param callback fun(response: lsp.CompletionResponse|nil)
 function source:complete(params, callback)
-	local core = require("cmp-pypi.core")
+    local core = require("cmp-pypi.core")
 
-	local line = params.context.cursor_before_line
+    local line = params.context.cursor_before_line
 
-	if not core.should_complete() then
-		return callback()
-	end
+    if not core.should_complete() then
+        return callback()
+    end
 
-	-- `package == version` for 0 to any number of spaces
-	local name, _ = string.match(line, '([%w_%-]+)%s*==%s*([^"= ]*)$')
+    -- `package == version` for 0 to any number of spaces
+    local name, _ = string.match(line, '([%w_%-]+)%s*==%s*([^"= ]*)$')
 
-	if not name then
-		-- `package = "version"` for 0 to any number of spaces
-		name, _ = string.match(line, '^([%w_%-]+)%s*=%s*"([^"]*)$')
+    if not name then
+        -- `package = "version"` for 0 to any number of spaces
+        name, _ = string.match(line, '^([%w_%-]+)%s*=%s*"([^"]*)$')
 
-		if not name then
-			return callback()
-		end
-	end
+        if not name then
+            return callback()
+        end
+    end
 
-	vim.schedule(function()
-		local result = core.complete(name)
-		callback(result)
-	end)
+    vim.schedule(function()
+        local result = core.complete(name)
+        callback(result)
+    end)
 end
 
 ---@param completion_item lsp.CompletionItem
 ---@param callback function
 function source:resolve(completion_item, callback)
-	callback(completion_item)
+    callback(completion_item)
 end
 
 ---@param completion_item lsp.CompletionItem
 ---@param callback function
 function source:execute(completion_item, callback)
-	callback(completion_item)
+    callback(completion_item)
 end
 
 return source
